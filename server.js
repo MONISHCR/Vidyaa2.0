6efc1c365170edcc3cd6ebee49fff5af87c0e9f9
Monish <monish21052004@gmail.com>
2024-07-27 22:25:00 +0530

    ss

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 const port = process.env.PORT || 4000;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
   })
